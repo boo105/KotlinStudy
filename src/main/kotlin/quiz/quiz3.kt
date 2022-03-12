2612f7511ddf2d3d294bf58a25c24d99791eb11d
boo105 <boo10500@naver.com>
2022-03-12 21:04:26 +0900

    로또 생성기(quiz3) 등수 구현

--- src/main/kotlin/quiz/quiz3.kt
+++ src/main/kotlin/quiz/quiz3.kt
@@ -1,5 +1,7 @@
 package quiz
 
+data class PredictLotto (val lotto : List<Int>)
+
 fun createLotto() : List<Int> {
     return (1..45).shuffled().take(7)
 }
@@ -14,12 +16,69 @@ fun printLotto(balls : List<Int>) {
     }
 }
 
+fun getLottoRank(predictLotto: List<Int>, answerLotto : List<Int>) : Int {
+    var count = 0;
+
+    for(lottoNumber in predictLotto) {
+        val findNumber = answerLotto.find {
+            it == lottoNumber
+        }
+
+        findNumber?.let { count++ }
+    }
+
+    val rank = when(count) {
+        6 -> {
+            val bonusNumber = answerLotto.get(answerLotto.size - 1)
+            val findBonusNumber = predictLotto.find { it == bonusNumber }
+            findBonusNumber?.let { 2 } ?: 1
+        }
+        5 -> 3
+        4 -> 4
+        3 -> 5
+        else -> -1
+    }
+
+    return rank
+}
+
 fun main() {
-    for(i in 1..5) {
+    val predicts : List<PredictLotto> = listOf(
+        PredictLotto(listOf(1,6,12,34,36,41)),
+        PredictLotto(listOf(1,7,13,22,31,43)),
+        PredictLotto(listOf(1,10,18,25,34,38)),
+        PredictLotto(listOf(2,11,14,16,25,38)),
+        PredictLotto(listOf(2,11,16,18,27,40)),
+        PredictLotto(listOf(4,7,12,27,35,44)),
+        PredictLotto(listOf(4,7,17,26,36,44)),
+    )
+
+
+    for(i in 1..40) {
         val balls : List<Int> = createLotto()
+
+//        val rankOne = balls.take(6)
+//        val rankTwo : MutableList<Int> = mutableListOf()
+//        rankTwo.add(balls.get(balls.size - 1))
+//        for(number in balls.take(6).shuffled().take(5)) {
+//            rankTwo.add(number)
+//        }
+
 
         println("-- ${i}회차 로또 번호 --")
         printLotto(balls)
         println()
+        predicts.forEachIndexed { index, predictLotto ->
+            val rank = getLottoRank(predictLotto.lotto, balls)
+            if (rank != -1)
+                println("${index + 1}번째 번호 : ${rank}등")
+            else
+                println("${index + 1}번째 번호 : 낙첨")
+
+        }
+//        println(rankOne)
+//        println(rankTwo)
+//        println("rankOne : ${getLottoRank(rankOne, balls)}등")
+//        println("rankTwo : ${getLottoRank(rankTwo, balls)}등")
     }
 }
